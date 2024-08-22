1ae50b86364fb957fe180d389bf65bf3007bf81d
Timothy Ogbemudia <ogbemudiatimothy@gmail.com>
2024-08-22 18:55:55 +0100

    export our fn

--- packages/hono-rpc-swr/src/api.ts
+++ packages/hono-rpc-swr/src/api.ts
@@ -1,21 +1,31 @@
-import useSWR from "swr";
-import useSWRMutation from "swr/mutation";
+import useSWR, { Key, SWRConfiguration } from "swr";
+import useSWRMutation, { SWRMutationConfiguration } from "swr/mutation";
 import { hc } from "hono/client";
 import { Hono } from "hono";
 type HTTPMethodSuffix = "$get" | "$post" | "$put" | "$patch" | (string & {});
-
-type SWRMethods = {
-  useSWR: (params?: any, options?: any) => any;
-  useSWRMutation: (options?: any) => any;
+type ParamsType = {
+  param?: Record<string, string>;
+  query?: Record<string, string>;
+  headers?: Record<string, string>;
 };
-type DeepPartialSWR<T> = T extends (...args: any[]) => any
-  ? T extends { [method in HTTPMethodSuffix]: Promise<any> }
-    ? T & SWRMethods
-    : T
-  : {
-      [K in keyof T]: DeepPartialSWR<T[K]>;
-    } & SWRMethods;
-export function createHonoRPCSWR<T extends Hono<any, any, any>>(
+
+type SWRMethods<T> = {
+  useSWR: (params?: ParamsType, options?: SWRConfiguration<T>) => any;
+  useSWRMutation: (options?: SWRMutationConfiguration<T, any>) => any;
+};
+
+type DeepPartialSWR<T> = T extends object
+  ? {
+      [K in keyof T]: T[K] extends (...args: any[]) => any
+        ? T[K] extends {
+            [method in HTTPMethodSuffix]: (...args: any[]) => Promise<any>;
+          }
+          ? T[K] & SWRMethods<Awaited<ReturnType<T[K][HTTPMethodSuffix]>>>
+          : T[K]
+        : DeepPartialSWR<T[K]>;
+    } & SWRMethods<T>
+  : T;
+function createHonoRPCSWR<T extends Hono<any, any, any>>(
   baseUrl: string
 ): DeepPartialSWR<T> {
   const client = hc<T>(baseUrl);
@@ -26,7 +36,7 @@ export function createHonoRPCSWR<T extends Hono<any, any, any>>(
       {
         get(target, prop: string) {
           if (prop === "useSWR") {
-            return (params?: any, options?: any) =>
+            return (params?: ParamsType, options?: SWRConfiguration) =>
               useSWR(
                 [...path],
                 () => (client as any)[path[0]][path[1]].$get(params),
@@ -34,10 +44,12 @@ export function createHonoRPCSWR<T extends Hono<any, any, any>>(
               );
           }
           if (prop === "useSWRMutation") {
-            return (options?: any) =>
+            return (
+              options?: SWRMutationConfiguration<any, any, Key, ParamsType>
+            ) =>
               useSWRMutation(
                 [...path],
-                (_, { arg }: { arg: any }) =>
+                (_: any, { arg }: { arg: any }) =>
                   (client as any)[path[0]][path[1]].$post({ form: arg }),
                 options
               );
@@ -50,3 +62,4 @@ export function createHonoRPCSWR<T extends Hono<any, any, any>>(
 
   return buildPath() as DeepPartialSWR<T>;
 }
+export { createHonoRPCSWR };
